4261ea9d2da9c6eb4be2dbfd7d7f0dc586b0fe8f
abhishek sharma <33564840+nobleblackk@users.noreply.github.com>
2019-11-05 12:22:07 +0530

    Update Insertion_Sort.py

--- Sorting_Algorithms/Insertion_Sort.py
+++ Sorting_Algorithms/Insertion_Sort.py
@@ -28,7 +28,7 @@ def Insertion_Sort(A):
     for i in range(1,len(A),1):
         if A[i]<A[i-1]:
             A[i],A[i-1]=A[i-1],A[i]
-            for j in range(i,0,-1):
+            for j in range(i-1,0,-1):
                 if A[j]<A[j-1]:
                     A[j],A[j-1]=A[j-1],A[j]
     print(A)        
